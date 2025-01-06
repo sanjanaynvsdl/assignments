08e4a4f25876e1c39cfed16b6df5a2248afdc16d
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2025-01-06 23:55:45 +0530

    Week-2 Assignment-1

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
-
+  isAnagramStr1= str1.toLowerCase().split("").sort().join("");
+  isAnagramStr2=str2.toLowerCase().split("").sort().join("");
+  console.log(isAnagramStr1);
+  console.log(isAnagramStr2);
+    if(isAnagramStr1===isAnagramStr2) {
+      return true;
+    }
+    return false;
 }
+const str = "Sanjana"
+const sorted = str.toLowerCase().split("").sort().join("");
+console.log(sorted);
+console.log(isAnagram("sanj", "jans"))
+// console.log()
 
 module.exports = isAnagram;
