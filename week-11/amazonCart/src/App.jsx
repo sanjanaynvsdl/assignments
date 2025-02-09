f7986e1701047e0f824dc6e1035bbbd51a75d217
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2025-02-09 12:20:21 +0530

    amazoncart done!

--- week-11/amazonCart/src/App.jsx
+++ week-11/amazonCart/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Layout from "./Components/Layout";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./Components/Pages/HomePage"
+import { RecoilRoot } from "recoil";
+import WishlistPage from "./Components/Pages/WishListPage";
+import CartPage from "./Components/Pages/CartPage"
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return(
+    <BrowserRouter>
+    <RecoilRoot>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+        <Route index  element={<HomePage/>}/>
+        <Route  path="/wishlist" element={<WishlistPage/>}/>
+        <Route path="/cart" element={<CartPage/>}/>
+        </Route>
+      </Routes>
+    </RecoilRoot>
+    </BrowserRouter>
+    
   )
 }
 
-export default App
+export default App;
